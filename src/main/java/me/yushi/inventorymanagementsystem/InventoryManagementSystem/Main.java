0f72ab7a51d1f1560d76da9d3b2851405652d3f3
Yushi Cui <realyushi@gmail.com>
2024-10-23 22:58:20 +1300

    ♻️ (Main.java, APP.java, CategoryView.java, DashboardView.java, InventoryTransactionView.java, ProductView.java, SupplierView.java): remove unused classes and refactor Main class to simplify application startup process
    📝 (Main.java): update main method to remove file handling logic and simplify application initialization
    
    ✨ (APP.java, CategoryView.java, DashboardView.java, InventoryTransactionView.java, ProductView.java, SupplierView.java): remove view classes to streamline the application and improve maintainability

--- src/main/java/me/yushi/inventorymanagementsystem/InventoryManagementSystem/Main.java
+++ src/main/java/me/yushi/inventorymanagementsystem/InventoryManagementSystem/Main.java
@@ -1,25 +1,9 @@
 package me.yushi.inventorymanagementsystem.InventoryManagementSystem;
 
-import java.io.IOException;
-import me.yushi.inventorymanagementsystem.view.APP;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        // File locations
-        String productFile = "product.json";
-        String categoryFile = "category.json";
-        String transationFile = "transation.json";
-        String supplierFile = "supplier.json";
-
-        try {
-            // Start the application, pass the file locations
-            APP app = new APP(categoryFile, transationFile, productFile, supplierFile);
-            app.start();
-        } catch (IOException ex) {
-            System.out.println("Error: " + ex.getMessage());
-        }
-
+    public static void main(String[] args) {
     }
 
 }
